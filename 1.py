2e657608f5763887641d2385732197d2f36d915a
Anji Reddy <akkalaanjireddy@gmail.com>
2026-07-21 19:55:08 +0530

    s

---
+++ 1.py
@@ -0,0 +1,55 @@
+
+
+name = "Anji Reddy "
+mobile = "9032029072"
+email = 'contact@softwareschool.co'
+
+price = 1000
+offer_percent = 10
+
+print( name  )
+
+print( "name" )
+
+print( mobile )
+
+print( 1000 * 0.1 )
+
+password = "dsdds63463__@##$"
+
+is_video_liked = True
+
+is_friend = False
+
+print( is_video_liked  )
+
+
+print( is_friend )
+
+
+course_1 = "reactjs"
+course_2 = "java"
+course_3 = "ai"
+
+# list data type
+courses = [ "reactjs", "java", "ai", "nodejs" ]
+
+print(courses)
+
+course_types = [ "recorded", "recorded", "live", "live", 258001, True  ]
+
+print( course_types )
+
+# dictionary data type
+course_1 = { "name": "reactjs", "type": "recorded"   }
+course_2 = { "name": "java", "type": "recorded" }
+course_3 = { "name": "ai", "type": "live"   }
+course_4 = { "name": "node", "type": "live" }
+
+print( course_3)
+
+print( course_2 )
+
+# if else, methods, APIs
+
+
